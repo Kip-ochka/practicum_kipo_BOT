90e9ceca2ba67792c93ebaa4a71ab4318081095d
Igor Kolpakov <kolpakovigorjob@yandex.ru>
2022-05-02 18:03:15 +0300

    пробую новую ветку

---
+++ command.js
@@ -0,0 +1,5 @@
+const command = `
+/star - приветсвие и перезапуск бота
+/help - помощь
+`
+module.exports.command = command
--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const { Telegraf } = require('telegraf')
 require('dotenv').config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
+const command = require('./command')
+
+
+bot.start((ctx) => ctx.reply('Привет, студент'))
+bot.help((ctx) => ctx.reply(command.command))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.on('message', (ctx) => console.log(ctx.message))
 bot.launch()
 
 // Enable graceful stop
